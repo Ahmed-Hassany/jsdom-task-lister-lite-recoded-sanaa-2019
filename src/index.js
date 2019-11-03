b6c9c1c5a70d717513a5cba1644c705464c53f09
Ahmed Hassan <ahmedalhassany1@gmail.com>
2019-11-03 20:42:36 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.getElementById("create-task-form").addEventListener("submit", function(Event){
+    const desc = document.getElementById("new-task-description").value;
+    let newItem = document.createElement("li");
+    newItem.innerText = desc;
+    document.getElementById("tasks").appendChild(newItem);
+    Event.preventDefault();
+  })
 });
